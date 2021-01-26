dbada9a41869c7ee7f3ed72d9a2d289fdfe8aa56
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-01-26 11:33:34 -0800

    added positions and other css values for position divs

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// game functions:
+console.log("this works");
+//start at first div
+//pop up with instructions ("Help Orion get home by answering the questions along his path!")
+//event listener for click on second div
+// function for creating random addition problem
+// input check ---> if correct answer, prompt pop up 'correct!' and move orion to next position
+// input check --> if incorrect answer, take half a life heart and prompt second chance, third, etc.                                         
+    // if all hearts exhausted, "Game over.  Would you like to try again?"
+    // if yes, return to start position
+// continue until last position
+// at home planet, pop up "Thanks for getting me home!"
+// possibly next level?
