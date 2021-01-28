d5b2305ca8f94ae6ab50c1d5c4b68f5f87a1e48e
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-01-27 16:05:52 -0800

    created functions for movement, still need to debug and polish

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+let lives= 3;
+
+document.querySelector("#livesLeft").innerHTML = "Lives: 3";
+
 // game functions:
 console.log("this works");
 //start at first div
@@ -14,47 +18,163 @@ console.log("page is fully loaded. this will turn into a pop up later")
 //  }
 // }
 //let lives = document.querySelector('#livesAnchor').innerHTML 
-
-//event listener for click on second div
-const position2 = document.querySelector('#position2')
-document.querySelector('#position2').addEventListener('click', () => {
+const handlePromptQuestion2 =  (e) => {
     console.log("position two works")
     let answer1 = prompt("4 * 4 =" );
-    console.log(answer1)
-    if (answer1 == "16") { 
+
+    //console.log(answer1);
+//  for let(i=0; i<3; i++) {
+        if (answer1 == "16") { 
         document.querySelector('#position2').style.opacity = 1;
         document.querySelector('#position1').style.opacity = 0;
-    } else if (answer1 !== "16") {
-        prompt("Oops! Not Quite. Try again.");
-        answer1
-        console.log(answer1)
-        
+        return
+        } else if (lives <= 1){
+            lives--;
+
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+            
+    } else  { 
+        lives--;
+
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
     }
-    //create if statement for if info == 16 then change opacity of div1 to 0 and opacity of div 2 to 1
-});
+};
+const handlePromptQuestion3 =  (e) => {
     
-document.querySelector('#position3').addEventListener('click', () => {
-    console.log("position three works")
+    let answer1 = prompt("5 * 9 =");
+        if (answer1 == "45") { 
+        document.querySelector('#position3').style.opacity = 1;
+        document.querySelector('#position2').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+
+const handlePromptQuestion4 =  (e) => {
+    let answer1 = prompt("3 * 7 =");
+        if (answer1 == "21") { 
+        document.querySelector('#position4').style.opacity = 1;
+        document.querySelector('#position3').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+
+const handlePromptQuestion5 =  (e) => {
     
-});document.querySelector('#position4').addEventListener('click', () => {
-    console.log("position four works")
+    let answer1 = prompt("6 * 8 =");
+        if (answer1 == "48") { 
+        document.querySelector('#position5').style.opacity = 1;
+        document.querySelector('#position4').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+const handlePromptQuestion6 =  (e) => {
     
-});document.querySelector('#position5').addEventListener('click', () => {
-    console.log("position five works")
+    let answer1 = prompt("2 * 9 =");
+        if (answer1 == "45") { 
+        document.querySelector('#position6').style.opacity = 1;
+        document.querySelector('#position5').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+const handlePromptQuestion7 =  (e) => {
     
-});document.querySelector('#position6').addEventListener('click', () => {
-    console.log("position six works")
+    let answer1 = prompt("4 * 7");
+        if (answer1 == "28") { 
+        document.querySelector('#position7').style.opacity = 1;
+        document.querySelector('#position6').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+const handlePromptQuestion8 =  (e) => {
     
-});document.querySelector('#position7').addEventListener('click', () => {
-    console.log("position seven works")
+    let answer1 = prompt(" 8 * 3");
+        if (answer1 == "24") { 
+        document.querySelector('#position8').style.opacity = 1;
+        document.querySelector('#position7').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+const handlePromptQuestion9 =  (e) => {
     
-});document.querySelector('#position8').addEventListener('click', () => {
-    console.log("position eight works")
+    let answer1 = prompt(" 3 * 6");
+        if (answer1 == "18") { 
+        document.querySelector('#position9').style.opacity = 1;
+        document.querySelector('#position8').style.opacity = 0;
+        return
+        } else if (lives <= 1){
+            lives--;
+    document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+            alert('Game Over!');
+            return
+    } else  { 
+        lives--;
+        document.querySelector('#livesLeft').innerHTML = `Lives: ${lives}`;
+        alert('Oops! Try again.');
+    }
+};
+const position2 = document.querySelector('#position2')
+document.querySelector('#position2').addEventListener('click', handlePromptQuestion2);
     
-});document.querySelector('#position9').addEventListener('click', () => {
-    console.log("position nine works")
-    
-});
+document.querySelector('#position3').addEventListener('click', handlePromptQuestion3);
+
+document.querySelector('#position4').addEventListener('click', handlePromptQuestion4);document.querySelector('#position5').addEventListener('click', handlePromptQuestion5);document.querySelector('#position6').addEventListener('click', handlePromptQuestion6);document.querySelector('#position7').addEventListener('click', handlePromptQuestion7);document.querySelector('#position8').addEventListener('click', handlePromptQuestion8);document.querySelector('#position9').addEventListener('click', handlePromptQuestion9);
 
 // function for creating random addition problem
 // input check ---> if correct answer, prompt pop up 'correct!' and move orion to next position
