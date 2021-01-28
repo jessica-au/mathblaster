2225d377d095c94c35fb38eae8c2a3ca50a8d038
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-01-28 11:42:49 -0800

    created classes for player container and indiv ids for each one, changed css and js to match

--- js/app.js
+++ js/app.js
@@ -6,19 +6,11 @@ const position2 = document.querySelector('#position2')
 // game functions:
 console.log("this works");
 //start at first div
-//pop up with instructions ("Help Orion get home by answering the questions along his path!")
 window.addEventListener('load', () => {
     console.log("page is fully loaded. this will turn into a pop up later")
 
-    //alert('Orion needs help!  Solve the math problems to help him get back to his home planet.')
+    alert('Orion needs help!  Solve the math problems to help him get back to his home planet.')
 });
-// function changeOpacity = () -> {
-// let startLocation = document.querySelector('position1')
-//  if (startLocation.style.opacity === 1) {
-//      startLocation.style.opacity = 0
-//  }
-// }
-//let lives = document.querySelector('#livesAnchor').innerHTML 
 const handlePromptQuestion2 = (e) => {
     console.log("position two works")
     let answer1 = prompt("4 * 4 =");
@@ -26,8 +18,8 @@ const handlePromptQuestion2 = (e) => {
     //console.log(answer1);
     //  for let(i=0; i<3; i++) {
     if (answer1 == "16") {
-        document.querySelector('#position2').style.opacity = 1;
-        document.querySelector('#position1').style.opacity = 0;
+        document.querySelector('#p2').style.opacity = 1;
+        document.querySelector('#p1').style.opacity = 0;
         document.querySelector('#position2').removeEventListener('click', handlePromptQuestion2);
         document.querySelector('#position3').addEventListener('click', handlePromptQuestion3);
         return
@@ -50,8 +42,8 @@ const handlePromptQuestion3 = (e) => {
 
     let answer1 = prompt("5 * 9 =");
     if (answer1 == "45") {
-        document.querySelector('#position3').style.opacity = 1;
-        document.querySelector('#position2').style.opacity = 0;
+        document.querySelector('#p3').style.opacity = 1;
+        document.querySelector('#p2').style.opacity = 0;
         document.querySelector('#position3').removeEventListener('click', handlePromptQuestion3);
         document.querySelector('#position4').addEventListener('click', handlePromptQuestion4);
         return
@@ -70,8 +62,8 @@ const handlePromptQuestion3 = (e) => {
 const handlePromptQuestion4 = (e) => {
     let answer1 = prompt("3 * 7 =");
     if (answer1 == "21") {
-        document.querySelector('#position4').style.opacity = 1;
-        document.querySelector('#position3').style.opacity = 0;
+        document.querySelector('#p4').style.opacity = 1;
+        document.querySelector('#p3').style.opacity = 0;
         document.querySelector('#position4').removeEventListener('click', handlePromptQuestion4);
         document.querySelector('#position5').addEventListener('click', handlePromptQuestion5);
         return
@@ -91,8 +83,8 @@ const handlePromptQuestion5 = (e) => {
 
     let answer1 = prompt("6 * 8 =");
     if (answer1 == "48") {
-        document.querySelector('#position5').style.opacity = 1;
-        document.querySelector('#position4').style.opacity = 0;
+        document.querySelector('#p5').style.opacity = 1;
+        document.querySelector('#p4').style.opacity = 0;
         document.querySelector('#position5').removeEventListener('click', handlePromptQuestion5);
         document.querySelector('#position6').addEventListener('click', handlePromptQuestion6);
         return
@@ -111,8 +103,8 @@ const handlePromptQuestion6 = (e) => {
 
     let answer1 = prompt("2 * 9 =");
     if (answer1 == "18") {
-        document.querySelector('#position6').style.opacity = 1;
-        document.querySelector('#position5').style.opacity = 0;
+        document.querySelector('#p6').style.opacity = 1;
+        document.querySelector('#p5').style.opacity = 0;
         document.querySelector('#position6').removeEventListener('click', handlePromptQuestion6);
         document.querySelector('#position7').addEventListener('click', handlePromptQuestion7);
         return
@@ -131,8 +123,8 @@ const handlePromptQuestion7 = (e) => {
 
     let answer1 = prompt("4 * 7");
     if (answer1 == "28") {
-        document.querySelector('#position7').style.opacity = 1;
-        document.querySelector('#position6').style.opacity = 0;
+        document.querySelector('#p7').style.opacity = 1;
+        document.querySelector('#p6').style.opacity = 0;
         document.querySelector('#position7').removeEventListener('click', handlePromptQuestion7);
         document.querySelector('#position8').addEventListener('click', handlePromptQuestion8);
         return
@@ -151,8 +143,8 @@ const handlePromptQuestion8 = (e) => {
 
     let answer1 = prompt(" 8 * 3");
     if (answer1 == "24") {
-        document.querySelector('#position8').style.opacity = 1;
-        document.querySelector('#position7').style.opacity = 0;
+        document.querySelector('#p8').style.opacity = 1;
+        document.querySelector('#p7').style.opacity = 0;
         document.querySelector('#position8').removeEventListener('click', handlePromptQuestion8);
         document.querySelector('#position9').addEventListener('click', handlePromptQuestion9);
         return
@@ -171,8 +163,8 @@ const handlePromptQuestion9 = (e) => {
 
     let answer1 = prompt("8 * 8");
     if (answer1 == "64") {
-        document.querySelector('#position9').style.opacity = 1;
-        document.querySelector('#position8').style.opacity = 0;
+        document.querySelector('#p9').style.opacity = 1;
+        document.querySelector('#p8').style.opacity = 0;
         document.querySelector('#position9').removeEventListener('click', handlePromptQuestion8);
         return
     } else if (lives <= 1) {
