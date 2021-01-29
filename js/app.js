7450e8e45d2082cfdac9b30cf0cb05d25e936630
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-01-28 18:16:36 -0800

    fixed part where i use opacity to give appearance of movement to actual append condition)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-const testMode = false;
+const testMode = true;
 let lives = 3;
 
+const orion = document.querySelector("#orion");
 document.querySelector("#livesLeft").innerHTML = "Lives: 3";
 const clickme = document.querySelector('#clickme');
 const elseConditions = () => { if (lives <= 1) {
@@ -46,8 +47,7 @@ const handlePromptQuestion2 = (e) => {
     //console.log("position two works")
     let answer1 = prompt("4 * 4 =");
     if (testMode || answer1 == "16") {
-        p2.style.opacity = 1;
-        p1.style.opacity = 0;
+        position2.append(orion);
         position3.append(clickme);
         position2.removeEventListener('click', handlePromptQuestion2);
         position3.addEventListener('click', handlePromptQuestion3);
@@ -63,8 +63,7 @@ const handlePromptQuestion3 = (e) => {
 
     let answer1 = prompt("5 * 9 =");
     if (testMode || answer1 == "45") {
-        p3.style.opacity = 1;
-        p2.style.opacity = 0;
+        position3.append(orion);
         position4.append(clickme);
         position3.removeEventListener('click', handlePromptQuestion3);
         position4.addEventListener('click', handlePromptQuestion4);
@@ -76,8 +75,7 @@ const handlePromptQuestion3 = (e) => {
 const handlePromptQuestion4 = (e) => {
     let answer1 = prompt("3 * 7 =");
     if (testMode || answer1 == "21") {
-        p4.style.opacity = 1;
-        p3.style.opacity = 0;
+        position4.append(orion);
         position5.append(clickme);
         position4.removeEventListener('click', handlePromptQuestion4);
         position5.addEventListener('click', handlePromptQuestion5);
@@ -90,8 +88,7 @@ const handlePromptQuestion5 = (e) => {
 
     let answer1 = prompt("6 * 8 =");
     if (testMode || answer1 == "48") {
-        p5.style.opacity = 1;
-        p4.style.opacity = 0;
+        position5.append(orion);
         position6.append(clickme);
         position5.removeEventListener('click', handlePromptQuestion5);
         position6.addEventListener('click', handlePromptQuestion6);
@@ -103,8 +100,7 @@ const handlePromptQuestion6 = (e) => {
 
     let answer1 = prompt("2 * 9 =");
     if (testMode || answer1 == "18") {
-        p6.style.opacity = 1;
-        p5.style.opacity = 0;
+        position6.append(orion);
         position7.append(clickme);
         position6.removeEventListener('click', handlePromptQuestion6);
         position7.addEventListener('click', handlePromptQuestion7);
@@ -116,8 +112,7 @@ const handlePromptQuestion7 = (e) => {
 
     let answer1 = prompt("4 * 7");
     if (testMode || answer1 == "28") {
-        p7.style.opacity = 1;
-        p6.style.opacity = 0;
+        position7.append(orion);
         position8.append(clickme);
         position7.removeEventListener('click', handlePromptQuestion7);
         position8.addEventListener('click', handlePromptQuestion8);
@@ -129,8 +124,7 @@ const handlePromptQuestion8 = (e) => {
 
     let answer1 = prompt(" 8 * 3");
     if (testMode || answer1 == "24") {
-        p8.style.opacity = 1;
-        p7.style.opacity = 0;
+        position8.append(orion);
         position9.append(clickme);
         position8.removeEventListener('click', handlePromptQuestion8);
         position9.addEventListener('click', handlePromptQuestion9);
@@ -142,8 +136,7 @@ const handlePromptQuestion9 = (e) => {
 
     let answer1 = prompt("8 * 8");
     if (testMode || answer1 == "64") {
-        p9.style.opacity = 1;
-        p8.style.opacity = 0;
+        position9.append(orion);
         clickme.style.opacity=0;
         position9.removeEventListener('click', handlePromptQuestion8);
         alert('Congratulations on getting Orion home and clearing level 1!');
